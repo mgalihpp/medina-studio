2d92415481439b305466f275440853c5bf4a8f11
mgalihpp <muhammadgalih451@gmail.com>
2025-04-28 09:12:57 +0700

    fix: update teacher bios for accuracy and detail in Pengajar component

--- app/features/tentang-kami/components/pengajar.tsx
+++ app/features/tentang-kami/components/pengajar.tsx
@@ -18,14 +18,14 @@ export function Pengajar() {
       id: 1,
       name: 'Sri Widiani',
       role: 'Instruktur Tata Rias dan Busana',
-      bio: 'Instruktur model dan tata rias di sekolah nasional dan internasional.',
+      bio: 'Lulusan dari Instituto Di Muda Burgo Milan, Italia dan telah berpengalaman mengajar lebih 6 tahun mengajar, termasuk di Paris de La Mode School, Italian Fashion School Jakarta, Instituto di Moda Burgo Jakarta dan lainnya.',
       image: '/img/mentor/widi.jpg',
     },
     {
       id: 2,
       name: 'Siska Handayani',
       role: 'Instruktur Henna',
-      bio: 'Henna Artist di berbagai jenis tata rias pengantin.',
+      bio: 'Henna Artist dengan pengalaman lebih dari 3 tahun dan menguasai berbagai jenis Henna, termasuk Henna Putih dan Henna Biru.',
       image: '/img/mentor/siska.jpg',
     },
   ];
